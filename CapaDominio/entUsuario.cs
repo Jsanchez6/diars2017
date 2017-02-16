505da5acc9ed3599d8665770c0e8b5ce198a30e4
Raed Miranda <lalo_sagitario.95@hotmail.es>
2017-02-16 18:49:11 -0500

    Files added by upload

---
+++ CapaDominio/entUsuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDominio
+{
+    public class entUsuario
+    {
+        public int idUsuario { get; set; }
+        public String Nombres { get; set; }
+        public String Apellidos { get; set; }
+        public String NombreUsuario { get; set; }
+        public String Clave { get; set; }
+        public DateTime ValidoHasta{ get; set; }
+    }
+}
